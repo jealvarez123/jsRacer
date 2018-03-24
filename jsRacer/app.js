bd224ec7199eaeac7a834dbbe0dc6ee81016bd35
jealvarez123 <jealvarez123@gmail.com>
2018-03-24 16:28:59 -0500

    added some small js code changes like calling the table

--- jsRacer/app.js
+++ jsRacer/app.js
@@ -1,66 +1,33 @@
 console.log("Can I kick it?");
 
+document.getElementsByClassName("race_track")
 
-
-// const theTrack = document.getElementsByClassName("table");
+// const players = [
 //
-// for (var i = 0, cell; cell = table.cells[i]; i++) {
-//      //iterate through cells
-//      //cells would be accessed using the "cell" variable assigned in the for loop
-//      console.log( table.cells[i]);
-// }
-
-// const moveUp = (event) => {
-// 	return event.target.dataset.played
-// }
 //
-// // this will go through all the pieces of road
+// ]
+//
+//
+// // // this will go through all the pieces of road
 // const buildArray = () => {
 // 	let road1 = [];
 // 	for(let i=0; i<table.length; i++) {
 // 		let move = squares[i].innerHTML;
 // 		track.push(move)
-// 	}
-// 	return road1;
-// }
 //
-// const playPiece = (table) => {
-// 	if(!squarePlayed(event)) {
-// 		square.innerHTML = playerPiece;
-// 		if (playerPiece === "") {
-// // 			playerPiece = "O";
-// // 		} else {
-// // 			playerPiece = "X";
-// // 		}
-// // 		square.dataset.played = "true";
-// // 		checkWinner();
-// // 	}
-// }
+document.addEventListener('keydown', (key1) => {
+
+  if (key1.which == 68) {
+
+    alert("d was pressed")
+  }
+
+});
 //
-//
-// // document.addEventListener('keydown', (key1) => {
-// //
-// //   if (key1.which == 68) {
-// //     alert("d was pressed")
-// //   }
-// //
-// // });
-//
-// // document.addEventListener('keydown', (key1) => {
-// //
-// //   if (key1.which == 39) {
-// //     alert("Left arrow was pressed")
-// //   }
-// //
-// // });
-//
-// for(let i=0; i < theTrack.length; i++) {
-// 	theTrack[i].addEventListener('keydown', (key1) => {
-//
-//     if (key1.which == 68) {
-//       alert("d was pressed")
-//     }
-//
-// 		// playPiece(event.target);
-// 	});
-// }
+document.addEventListener('keydown', (key1) => {
+
+  if (key1.which == 39) {
+    alert("Left arrow was pressed")
+  }
+
+});
