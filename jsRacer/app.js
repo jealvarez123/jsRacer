8ebb2e98784830fbdb4848e6d9486fece780e319
jealvarez123 <jealvarez123@gmail.com>
2018-03-24 12:46:53 -0500

    Got the code to recognize the key presses

--- jsRacer/app.js
+++ jsRacer/app.js
@@ -1,1 +1,17 @@
 console.log("Can I kick it?");
+
+document.addEventListener('keydown', (key1) => {
+
+  if (key1.which == 68) {
+    alert("d was pressed")
+  }
+
+});
+
+document.addEventListener('keydown', (key1) => {
+
+  if (key1.which == 39) {
+    alert("Left arrow was pressed")
+  }
+
+});
