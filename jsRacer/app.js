0e50627d128039141404c709ac0098271eb8e516
jealvarez123 <jealvarez123@gmail.com>
2018-03-24 14:56:49 -0500

    cleaned up the css

--- jsRacer/app.js
+++ jsRacer/app.js
@@ -1,17 +1,66 @@
 console.log("Can I kick it?");
 
-document.addEventListener('keydown', (key1) => {
 
-  if (key1.which == 68) {
-    alert("d was pressed")
-  }
 
-});
+// const theTrack = document.getElementsByClassName("table");
+//
+// for (var i = 0, cell; cell = table.cells[i]; i++) {
+//      //iterate through cells
+//      //cells would be accessed using the "cell" variable assigned in the for loop
+//      console.log( table.cells[i]);
+// }
 
-document.addEventListener('keydown', (key1) => {
-
-  if (key1.which == 39) {
-    alert("Left arrow was pressed")
-  }
-
-});
+// const moveUp = (event) => {
+// 	return event.target.dataset.played
+// }
+//
+// // this will go through all the pieces of road
+// const buildArray = () => {
+// 	let road1 = [];
+// 	for(let i=0; i<table.length; i++) {
+// 		let move = squares[i].innerHTML;
+// 		track.push(move)
+// 	}
+// 	return road1;
+// }
+//
+// const playPiece = (table) => {
+// 	if(!squarePlayed(event)) {
+// 		square.innerHTML = playerPiece;
+// 		if (playerPiece === "") {
+// // 			playerPiece = "O";
+// // 		} else {
+// // 			playerPiece = "X";
+// // 		}
+// // 		square.dataset.played = "true";
+// // 		checkWinner();
+// // 	}
+// }
+//
+//
+// // document.addEventListener('keydown', (key1) => {
+// //
+// //   if (key1.which == 68) {
+// //     alert("d was pressed")
+// //   }
+// //
+// // });
+//
+// // document.addEventListener('keydown', (key1) => {
+// //
+// //   if (key1.which == 39) {
+// //     alert("Left arrow was pressed")
+// //   }
+// //
+// // });
+//
+// for(let i=0; i < theTrack.length; i++) {
+// 	theTrack[i].addEventListener('keydown', (key1) => {
+//
+//     if (key1.which == 68) {
+//       alert("d was pressed")
+//     }
+//
+// 		// playPiece(event.target);
+// 	});
+// }
