2489c9cc5a9814d54bcbd604b0cf47c6ade611b3
jealvarez123 <jealvarez123@gmail.com>
2018-03-25 17:35:18 -0500

    Last commit for the day

--- jsRacer/app.js
+++ jsRacer/app.js
@@ -1,75 +1,50 @@
 console.log("Can I kick it?");
 
-// document.getElementsByClassName("race_track")
-//
-//
-// const buildArray = () => {
-//   let track = [];
-//   for (var i = 0; i < race_track.length; i++) {
-//     race_track[i]
-//   }
-// }
-//
-// const moveCar = () => {
 
 
-// const players = [
-//
-//
-// ]
-//
-//
-// // // this will go through all the pieces of road
-// const buildArray = () => {
-// 	let road1 = [];
-// 	for(let i=0; i<table.length; i++) {
-// 		let move = squares[i].innerHTML;
-// 		track.push(move)
-//
+// this calls the Container
+var Container = document.getElementById("player1_strip");
 
-// Get the container element
-var btnContainer = document.getElementById("player1_strip");
+// this calls the class
+var line1 = Container.getElementsByClassName("mile");
 
-// Get all buttons with class="btn" inside the container
-var line1 = btnContainer.getElementsByClassName("mile");
-
-// Loop through the buttons and add the active class to the current/clicked button
+// this replaces the mile tables with active1
 for (var i = 0; i < line1.length; i++) {
-  line1[i].addEventListener("click", function() {
+  line1[i].addEventListener('keydown',(key1) => {
+    if (key1.which == 68){
+      console.log("this works");
     var current = document.getElementsByClassName("active1");
     current[0].className = current[0].className.replace(" active1", "");
-    this.className += " active1";
+    current[i].className += " active1";
+  }
   });
 }
 
-var btnContainer = document.getElementById("player2_strip");
+// this calls the Container
+var Container = document.getElementById("player2_strip");
 
-// Get all buttons with class="btn" inside the container
-var line2 = btnContainer.getElementsByClassName("mile2");
+// this calls the class
+var line2 = Container.getElementsByClassName("mile2");
 
-// Loop through the buttons and add the active class to the current/clicked button
+
 for (var i = 0; i < line1.length; i++) {
-  line2[i].addEventListener("click", function() {
+  line2[i].addEventListener("keydown", function(key1) {
+    if (key1.which == 39)
+
+
     var current = document.getElementsByClassName("active2");
-    current[0].className = current[0].className.replace(" active2", "");
+    current[0].className = current[0].className.replace(" active2", "").next();
     this.className += " active2";
   });
 }
 
 
-document.addEventListener('keydown', (key1) => {
 
-  if (key1.which == 68) {
 
-    alert("d was pressed")
-  }
-
-});
+// document.addEventListener('keydown', (key1) => {
 //
-document.addEventListener('keydown', (key1) => {
-
-  if (key1.which == 39) {
-    alert("Left arrow was pressed")
-  }
-
-});
+//   if (key1.which == 39) {
+//     alert("Left arrow was pressed")
+//   }
+//
+// });
