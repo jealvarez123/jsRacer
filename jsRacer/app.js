5536539003957b6b33669f868231f817dfb10b44
Jaime Alvarez <jealvarez123@gmail.com>
2018-09-10 14:29:09 -0500

    testing upload

--- jsRacer/app.js
+++ jsRacer/app.js
@@ -3,40 +3,59 @@ console.log("Can I kick it?");
 
 
 // this calls the Container
-var Container = document.getElementById("player1_strip");
+let container = document.getElementById("player1_strip");
+
+for (var i = 0; i < player1.length; i++) {
+  let
+}
+console.log(container);
+
+let active1 =
+document.getElementById('active1')
+console.log(active1);
+
 
 // this calls the class
-var line1 = Container.getElementsByClassName("mile");
+// let line1 = Container.getElementsByClassName();
+//
+//
+//
+//
+// for (var i = 0; i < player1_strip.length; i++) {
+//   let move1 =
+// }
+
+
 
 // this replaces the mile tables with active1
-for (var i = 0; i < line1.length; i++) {
-  line1[i].addEventListener('keydown',(key1) => {
-    if (key1.which == 68){
-      console.log("this works");
-    var current = document.getElementsByClassName("active1");
-    current[0].className = current[0].className.replace(" active1", "");
-    current[i].className += " active1";
-  }
-  });
-}
-
-// this calls the Container
-var Container = document.getElementById("player2_strip");
-
-// this calls the class
-var line2 = Container.getElementsByClassName("mile2");
-
-
-for (var i = 0; i < line1.length; i++) {
-  line2[i].addEventListener("keydown", function(key1) {
-    if (key1.which == 39)
-
-
-    var current = document.getElementsByClassName("active2");
-    current[0].className = current[0].className.replace(" active2", "").next();
-    this.className += " active2";
-  });
-}
+// for (var i = 0; i < line1.length; i++) {
+//   line1[i].addEventListener('keydown',(key1) => {
+//     if (key1.which == 68){
+//       console.log("this works");
+//     var current = document.getElementsByClassName("active1");
+//     current[0].className = current[0].className.replace(" active1", "");
+//     current[i].className += " active1";
+//   }
+//   });
+// }
+//
+// // this calls the Container
+// var Container = document.getElementById("player2_strip");
+//
+// // this calls the class
+// var line2 = Container.getElementsByClassName("mile2");
+//
+//
+// for (var i = 0; i < line1.length; i++) {
+//   line2[i].addEventListener("keydown", function(key1) {
+//     if (key1.which == 39)
+//
+//
+//     var current = document.getElementsByClassName("active2");
+//     current[0].className = current[0].className.replace(" active2", "").next();
+//     this.className += " active2";
+//   });
+// }
 
 
 
